73d7e30b3cd2ed11e9290c142df3449531eff0dc
Bret Farley <bretfarley12@mittymonarchl.com>
2018-04-22 02:59:03 -0700

    fixed mailer

--- server.js
+++ server.js
@@ -4,37 +4,53 @@ var bodyParser = require('body-parser');
 var path = require('path');
 var nodemailer = require('nodemailer');
 var favicon = require('serve-favicon');
+var config = require('./config.json');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, './public')));
 app.set('views', path.join(__dirname, './views'));
 app.use(favicon(path.join(__dirname,'public','images','favicon.ico')));
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+
+
 const transporter = nodemailer.createTransport({
-    host: 'smtp.ethereal.email',
-    port: 587,
+    service: "Gmail",
     auth: {
-        user: 'spzvvmykeg2unamz@ethereal.email',
-        pass: 'YRGpzZujas2H5WMxkW'
+        user: config.user,
+        pass: config.pass
     }
 });
 
 app.get('/', function(req, res) {
+    console.log('config_________________');
+    console.log(config);
     res.sendFile(path.join(__dirname + '/views/index.html'));
 });
 
 app.post('/sendform', function(req, res) {
-    console.log('form sent');
-    console.log(req.body);
     let mailBody = {
-        from: req.body.email,
+        from: 'BretBot <fretbarleybot@gmail.com>',
         to: 'bretfarley129@gmail.com',
         subject: `${req.body.name} contacted you!`,
-        text: req.body.message
+        html: `
+        <h3> Name: ${req.body.name} </h3>
+        <h3> Email: ${req.body.email} </h3>
+        <h3> Phone: ${req.body.phone} </h3>
+        <div> ${req.body.message} </div>
+        `
     };
     transporter.sendMail(mailBody, function (err, info) {
-        console.log("err", err)
-        console.log("info", info)
+        if (err){
+            console.log("err", err)
+        }
+        else{
+            console.log("info", info)
+            console.log('Message sent: %s', info.messageId);
+            // Preview only available when sending through an Ethereal account
+            console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+        }
     })
     res.redirect('/');
 });
